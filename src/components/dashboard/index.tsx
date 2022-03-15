efcc2dbe8e7efbaae44d69145640c07a17e21fdf
André Luis <anddreluis98@gmail.com>
2022-03-15 15:07:30 -0300

    criação do dashboard

---
+++ src/components/dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from "./styles";
+import { Summary } from "../Summary/Index";
+
+export function Dashboard() {
+    return(
+        <Container>
+            <Summary />
+        </Container>
+    )
+}
