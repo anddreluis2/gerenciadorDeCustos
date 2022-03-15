5436df4671fa4e87d5635da9cf29dd17dff7ad95
André Luis <anddreluis98@gmail.com>
2022-03-15 17:16:00 -0300

    componentização finalizada

--- src/components/dashboard/index.tsx
+++ src/components/dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Container } from "./styles";
 import { Summary } from "../Summary/Index";
+import { TransactionsTable } from "../TransactionsTable";
 
 export function Dashboard() {
     return(
         <Container>
             <Summary />
+            <TransactionsTable />
         </Container>
     )
 }
